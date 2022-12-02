ad04cd122901897ad4ad978101bafd2caf6abda8
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-12-02 09:16:43 -0500

    lab 81 and 82 finished

---
+++ lab_8-1.py
@@ -0,0 +1,7 @@
+#author: RED 12/2/22
+sum=0
+integer= int(input("Input an integer"))
+#making for loop
+for number in range(integer):
+    sum += number
+print(sum)
---
+++ lab_8-2.py
@@ -0,0 +1,5 @@
+#author:RED 12/2/22
+names=["Rob","Jack","Ethan"]
+for name in names:
+    put= "Hi " + name + ", "
+    print(put + "You're invited to my party on Friday!")
