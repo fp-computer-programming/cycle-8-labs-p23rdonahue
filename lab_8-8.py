f09b542e813e266ed700601360fe0a03a69edc55
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-12-09 14:28:07 -0500

    changes

--- lab_8-8.py
+++ lab_8-8.py
@@ -4,10 +4,10 @@ rows = [["Darick", "Eugene", "Kyle", "Azaan"], ["Ryan",
 ["Christian", "Josh", "Matt", "Francesco"],
 ["Patrick", "Nico", "Trevayne"]]
 
-for row in rows:
-    for names in row:
-        if names[-1] == "s":
+for row in rows:#making loop
+    for names in row:#making loop so it prints individua anmes
+        if names[-1] == "s":#if the names end in s add '
             names+="'"
         else:
             names+="'s"
-        print(names)
+        print(names)#print
